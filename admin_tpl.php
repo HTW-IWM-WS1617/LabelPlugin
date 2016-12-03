60151591107154070969a7ef6ae1a5483fc80ca2
ggraterol <graterolgrecia@gmail.com>
2016-12-03 13:17:24 +0100

    Admin-Menu V1

---
+++ admin_tpl.php
@@ -0,0 +1,46 @@
+<h1><?php echo $this->getLang('admin headline')?></h1>
+
+<form action="<?php echo script()?>" method="post">
+    <div class="no">
+        <input type="hidden" name="do" value="admin" />
+        <input type="hidden" name="page" value="htwlabel" />
+        <input type="hidden" name="id" value="<?php echo hsc($ID)?>" />
+        <input type="hidden" name="sectok" value="<?php echo hsc(getSecurityToken())?>" />
+    </div>
+
+<table class="inline">
+    <tr>
+        <th><?php echo $this->getLang('admin label name')?></th>
+        <th><?php echo $this->getLang('admin label color')?></th>
+        <th><?php echo $this->getLang('admin icon')?></th>
+        <th><?php echo $this->getLang('admin action')?></th>
+    </tr>
+    <tr>
+        <td><input type="text" name="newlabel[name]" class="edit" /></td>
+        <td><input type="color" name="newlabel[color]" class="edit" /></td>
+        <td><input type="" name="newlabel[icon]" class="edit" /></td>
+        <td><input type="submit" class="button" name="action[create]" value="<?php echo $this->getLang('admin create')?>" /></td>
+    </tr>
+<?php //foreach ($labels as $label => $opts): ?>
+<?php //$label = hsc($label); ?>
+    <tr>
+        <td>
+            <input class="edit" type="text" value="<?php echo $label ?>" name="labels[<?php echo $label ?>][name]" />
+        </td>
+        <td>
+            <input class="edit" style="color: <?php echo $opts['color'] ?>" type="color"
+                value="<?php echo $opts['color'] ?>" name="labels[<?php echo $label ?>][color]" />
+        </td>
+        <td>
+            <input class="edit" type="text" value="<?php echo $opts['icon'] ?>" name="labels[<?php echo $label ?>][icon]" />
+        </td>
+        <td>
+            <input type="submit" name="action[delete][<?php echo $label ?>]" class="button"
+                value="<?php echo $this->getLang('admin delete')?>" />
+        </td>
+    </tr>
+<?php //endforeach; ?>
+</table>
+
+<input type="submit" name="action[save]" value="<?php echo $this->getLang('admin save')?>" class="button" />
+</form>
